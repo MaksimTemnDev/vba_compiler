4e16d7f1cbec250b3b3eb7c3faa50c9dcc5df3ea
Temnenkov Maxim <temnenkov.m@roistat.com>
2024-02-12 09:55:41 +0300

    add test 8

---
+++ prog8.vb
@@ -0,0 +1,16 @@
+
+Function Subfunction(ByRef ByVal art() As Double)
+	Dim a = 5
+	'Static arrayVal(1,2) = { }
+	DO While a < 10
+		a = a \= (2 + 1)*1
+	LOOP
+	
+	Do UNTIL a > 10
+		a = a + (2 + 1)*1
+		EXIT Do;
+	LOOP
+	
+	Dim ces = TRUE
+	Return ces
+End Function
