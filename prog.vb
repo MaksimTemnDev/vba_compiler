da34797bcda79b5a92df97ae6d9dbc3a35761674
Temnenkov Maxim <temnenkov.m@roistat.com>
2024-02-13 21:11:10 +0300

    fix test 0

--- prog.vb
+++ prog.vb
@@ -1,3 +1,3 @@
 Sub teon()
-	Dim grayjoy = “A wise man once told me that winter comes for all men, in their time.”
+	Dim grayjoy = "A wise man once told me that winter comes for all men, in their time."
 End Sub
